6c9c17c74c57462078e15380a088602ecb5b0a5f
Torben Hansen <derhansen@gmail.com>
2014-05-01 14:27:58 +0200

    [TASK] Remove caching Framework stuff for TYPO3 4.5 - Closes #116

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,24 +19,3 @@ if (!defined('TYPO3_MODE')) {
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'])) {
 	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'] = array();
 }
-// Define string frontend as default frontend, this must be set with TYPO3 4.5 and below
-// and overrides the default variable frontend of 4.6
-if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'] = 't3lib_cache_frontend_StringFrontend';
-}
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < '4006000') {
-	// Define database backend as backend for 4.5 and below (default in 4.6)
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'] = 't3lib_cache_backend_DbBackend';
-	}
-	// Define data and tags table for 4.5 and below (obsolete in 4.6)
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options'] = array();
-	}
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options']['cacheTable'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options']['cacheTable'] = 'cf_sfbanners_cache';
-	}
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options']['tagsTable'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['options']['tagsTable'] = 'cf_sfbanners_cache_tags';
-	}
-}
