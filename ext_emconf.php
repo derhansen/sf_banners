8ca0a27066295137ddc5cac9d24b6d273cc66cf6
Torben Hansen <derhansen@gmail.com>
2015-09-08 08:56:37 +0200

    [TASK] Updated compatibility to TYPO3 7.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'extbase' => '6.2',
 			'fluid' => '6.2',
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
