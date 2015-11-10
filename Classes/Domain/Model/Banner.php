062044689401a51f9364f82ca5393329fe7640b6
Henri Nathanson <henri.nathanson@gmail.com>
2015-11-10 12:16:21 +0100

    dont like makeInstance('TYPO3\\CMS...') double backslash notation

--- Classes/Domain/Model/Banner.php
+++ Classes/Domain/Model/Banner.php
@@ -708,7 +708,7 @@ class Banner extends AbstractEntity {
 	 * @return mixed
 	 */
 	public function getLinkUrl() {
-		$cObj = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
+		$cObj = GeneralUtility::makeInstance(\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer::class);
 		return $cObj->getTypoLink_URL($this->link);
 	}
 
