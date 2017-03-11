28ccbee603e5a86e8ca7afc3536e566743acbf9c
Torben Hansen <derhansen@gmail.com>
2017-03-11 16:04:50 +0100

    Added defaults for caching framework

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,3 +17,16 @@ defined('TYPO3_MODE') or die();
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'])) {
     $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'] = [];
 }
+
+if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'])) {
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'] = \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class;
+}
+
+if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'])) {
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'] = TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class;
+}
+
+// Set Cache groups
+if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'])) {
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'] = ['pages', 'all'];
+}
