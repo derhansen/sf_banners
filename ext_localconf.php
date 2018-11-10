4674c6d7c310726bd81a0dcd64f8fd894b8b677f
Torben Hansen <derhansen@gmail.com>
2018-11-10 13:25:04 +0100

    [BUGFIX] Fixed TYPO3 Caching Framework integration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,32 +1,34 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'DERHANSEN.' . $_EXTKEY,
-    'Pi1',
-    [
-        'Banner' => 'show,getBanners,click',
-    ],
-    /* non-cacheable actions */
-    [
-        'Banner' => 'getBanners,click',
-    ]
-);
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'DERHANSEN.sf_banners',
+        'Pi1',
+        [
+            'Banner' => 'show,getBanners,click',
+        ],
+        /* non-cacheable actions */
+        [
+            'Banner' => 'getBanners,click',
+        ]
+    );
 
-// Register cache 'sfbanners_cache'
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache'] = [];
-}
+    // Register cache 'sfbanners_cache'
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache'] = [];
+    }
 
-if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'] = \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class;
-}
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['frontend'] = \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class;
+    }
 
-if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'] = TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class;
-}
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['backend'] = TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class;
+    }
 
-// Set Cache groups
-if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'] = ['pages', 'all'];
-}
+    // Set Cache groups
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sfbanners_cache']['groups'] = ['pages', 'all'];
+    }
+});
