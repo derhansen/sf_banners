405ceca8e56c2470722d5fbfda4f0699e1fd0323
Torben Hansen <derhansen@gmail.com>
2021-08-15 12:54:55 +0200

    [TASK] Remove management DB fields from ext_tables.sql
    Refs #224

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,6 @@
 # Table structure for table 'tx_sfbanners_domain_model_banner'
 #
 CREATE TABLE tx_sfbanners_domain_model_banner (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	title varchar(255) DEFAULT '' NOT NULL,
 	description varchar(255) DEFAULT '' NOT NULL,
 	type int(11) DEFAULT '0' NOT NULL,
@@ -22,40 +18,6 @@ CREATE TABLE tx_sfbanners_domain_model_banner (
 	clicks int(11) DEFAULT '0' NOT NULL,
 	excludepages int(11) DEFAULT '0' NOT NULL,
 	recursive tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-	fe_group varchar(100) DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sorting int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-	KEY language (l10n_parent,sys_language_uid),
-	KEY phpunit_dummy (is_dummy_record)
-
 );
 
 #
@@ -71,8 +33,7 @@ CREATE TABLE tx_sfbanners_domain_model_banner_excludepages_mm (
 	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
 
 	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign),
-	KEY phpunit_dummy (is_dummy_record)
+	KEY uid_foreign (uid_foreign)
 );
 
 #
