b7a3cea5af8edc94f7ffada35f371dbc9b1597bb
Napora <krzysztof.napora@drblitz-weblab.com>
2023-03-03 12:31:13 +0100

    Cleanup

--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,2 @@
 <?php
 defined('TYPO3') or die();
-
-call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-        'tx_sfbanners_domain_model_banner',
-        'EXT:sf_banners/Resources/Private/Language/locallang_csh_tx_sfbanners_domain_model_banner.xlf'
-    );
-});
