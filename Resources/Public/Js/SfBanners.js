f759f15ac8fa28370f9cee696d74266100126d07
Torben Hansen <derhansen@gmail.com>
2023-04-27 07:39:13 +0200

    [BUGFIX] Only execute JavaScript when banners available
    Refs #250
    
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Resources/Public/Js/SfBanners.js
+++ Resources/Public/Js/SfBanners.js
@@ -4,20 +4,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   let url = ''
   let formData = new FormData();
+  let hasBanners = false;
 
   [...document.querySelectorAll('[id^="banner-"]')].forEach(banner => {
     url = banner.dataset.fetchurl
+    hasBanners = true;
     formData.append('tx_sfbanners_pi1[bannerConfigs][]', banner.dataset.config)
   });
 
-  fetch(url, { body: formData, method: 'POST' })
-    .then((resp) => resp.json())
-    .then(function (data) {
-      for (let i = 0; i < data.length; i++) {
-        postscribe('#banner-' + data[i]['uniqueId'], data[i]['html'])
-      }
-    })
-    .catch(function (error) {
-      console.log(error)
-    });
+  if (hasBanners) {
+    fetch(url, { body: formData, method: 'POST' })
+      .then((resp) => resp.json())
+      .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+          postscribe('#banner-' + data[i]['uniqueId'], data[i]['html'])
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      });
+  }
 })
